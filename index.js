df035196a33a9f96eba04106915d5fb680781747
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2022-04-18 19:16:25 +0300

    removed debug lines

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const routes = require('./routes');
 swaggerDocument = require("./swagger.json");
 const swaggerUI = require("swagger-ui-express");
 
-const userService = require('./Services/user.service')
-
 const app = express();
 app.use(express.json());
 app.use(routes);
@@ -17,6 +15,5 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, async () => {
     await db.connect();
-    console.log(await userService.getUser({email: "bla@gmail.com"}))
     console.log(`server open at http://localhost:${port}`);
 })
