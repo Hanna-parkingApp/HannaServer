addb7abb16a03c4598409a1ed1d6e7c761a0afe0
hanna_user <dbn290895@gmail.com>
2022-04-25 00:21:47 +0300

    commit

--- Controllers/App/ShareParkController.js
+++ Controllers/App/ShareParkController.js
@@ -1,12 +1,18 @@
 const createParkingTransaction = require('../../Services/parkingTransaction.service'); 
 
 async function shareParkController(req, res) {
-    try {
-         
-         
- } catch (e) {
- 
-    }
+   try {
+      const { specificLocation,generalLocation, arrivalTime } = req.body;
+
+      if (!(specificLocation && generalLocation && arrivalTime)) {
+          return res.status(400).json({message: "Failed to connect due to empty parametres"});
+      }
+
+      const transactions = await getParkingTransaction({timeStamp: {$gte: arrivalTime , $lte : arrivalTime + 4 * 60000 }})
+      console.log(req.body);
+} catch (e) {
+
  }
+}
  
  module.exports = shareParkController;
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ const registerController = require('../Controllers/Auth/Register');
 const loginController = require('../Controllers/Auth/Login');
 const parkingSearcherController = require('../Controllers/App/ParkingSearcherController');
 const auth = require('../Middleware/Auth');
+const shareParkController = require('../Controllers/App/shareParkController');
 
 router.get('/', auth, (req, res) => {res.status(200).json('hello')})
-router.get('/share-park')
+// router.get('/share-park')
 
 router.post('/register', registerController);
 router.post('/login', loginController);
 router.post('/find-parks', parkingSearcherController);
+router.post('/share-parks', shareParkController);
 
 module.exports = router;
