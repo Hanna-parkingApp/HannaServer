f9413b380bd0d8d6c178f350ab12f644aaae4e0a
hanna_user <dbn290895@gmail.com>
2022-04-26 11:51:22 +0300

    done added data to db

--- Controllers/App/ShareParkController.js
+++ Controllers/App/ShareParkController.js
@@ -1,5 +1,6 @@
 const {getUser}   = require('../../Services/user.service');
 const {createUserParking} = require('../../Services/userParking.service');
+const User = require('../../db/schemas/User');
 
 
 async function shareParkController(req, res) {
@@ -12,27 +13,26 @@ async function shareParkController(req, res) {
     if (!(userToken && specificLocation && genralLocation && timeStamp )) {
         return res.status(400).json({ message:"All Fields must be provided"})
     }
-    const specificLocation_json = JSON.stringify(specificLocation);
+    const specificLocation_json = JSON.stringify(spaecificLocation);
 
     const user = await getUser({token: userToken});
-    console.log("!!!!!!!!!!!!!!!",user);
+    // console.log("!!!!!!!!!!!!!!!",user);
 
     if(user==null)
     {
         return res.status(500).json({ message:"user not found in DB"})
     }
-    let userID = user.id;
-    console.log("@@@@@",userID);
+    let userID = user[0]._id;
+    // console.log("@@@@@",userID);
     try{
 
     const newParkingUSER = await createUserParking({
-        userID,
+        userId: userID ,
         specificLocation: specificLocation_json,
         generalLocation: genralLocation,
         timeStamp 
     });
-    return res.status(200).json({
-        "message": "user parking successfully.",
+    return res.status(200).json({"message": "user parking successfully.",
     });
     }
     catch(e){
