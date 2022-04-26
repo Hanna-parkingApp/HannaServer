5ffdd81978723d9860390331334456d4ffce04eb
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2022-04-26 20:19:55 +0300

    auth middleware refactore

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 const config = process.env;
+const refreshToken = require('../Services/token.service')
+const userService = require('../services/user.service');
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
     const token = req.body.token || req.query.token || req.headers["x-access-token"];
 
     if (!token) {
@@ -13,6 +15,17 @@ const verifyToken = (req, res, next) => {
         req.user = decoded;
     }
     catch (err) {
+        if (err["expiredAt"] < Date.now()) {
+            if (!req.headers["x-refresh-token"]) {
+                return res.status(400).send("Refresh token is required");
+            }
+            const userArray = userService.getUser({token: req.headers["x-refresh-token"]});
+            const tokens = await refreshToken.refreshToken(req.headers["x-refresh-token"], userArray[0]);
+            req.headers["x-refresh-token"] = tokens.refreshToken;
+            req.headers["x-access-token"] = tokens.accessToken;
+            req.user = userArray[0];
+            return next();
+        }
         return res.status(400).send("invalid token");
     }
     return next();
