252cc92d244bcc93aa2cddecd2413d791b9645e6
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2022-05-04 18:47:50 +0300

    recover password helper functions and routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const registerController = require('../Controllers/Auth/Register');
 const loginController = require('../Controllers/Auth/Login');
 const parkingSearcherController = require('../Controllers/App/ParkingSearcherController');
+const registerController = require('../Controllers/Auth/Register');
+const {generateRecoveryCode, verifyRecoveryCode, changePassword} = require('../Controllers/Auth/RecoverPassword');
 const shareParkController = require('../Controllers/App/ShareParkController');
 
 const auth = require('../Middleware/Auth');
 
 router.get('/', auth, (req, res) => {res.status(200).json('hello')})
-// router.get('/share-park')
 
-router.post('/register', registerController);
-router.post('/login', loginController);
+router.post('/changePassword', changePassword)
 router.post('/find-parks', parkingSearcherController);
+router.post('/generateRecoveryCode', generateRecoveryCode);
+router.post('/login', loginController);
+router.post('/register', registerController);
 router.post('/share-parks', shareParkController);
+router.post('/verifyRecoveryCode', verifyRecoveryCode);
 
 
 module.exports = router;
