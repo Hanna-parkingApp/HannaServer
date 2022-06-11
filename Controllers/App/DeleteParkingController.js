188c97d5368be99f217bd624231d828af0a3dfcd
hanna_user <dbn290895@gmail.com>
2022-06-11 19:33:08 +0300

    commit route

---
+++ Controllers/App/DeleteParkingController.js
@@ -0,0 +1,37 @@
+const { getUser } = require('../../Services/user.service');
+const { deleteUserParking } = require('../../Services/userParking.service')
+
+async function deleteParkingController (req,res) {
+    console.log("DeleteParkingController: delete parking by the user id");
+
+    try {
+        console.log('req body: ', req.body);
+        const { userParkingId } = req.body;
+        if (!userParkingId) {
+            return res.status(400).json({ message:"User parkingId must be provided"})
+        }
+
+        // const parkingList = await getUserParking({ userId })
+        // if (!parkingList) {
+        //     return res.status(400).json({ message:"User have no parkings"})
+        // }
+
+        // const userParkingId = parkingList[parkingList.length - 1]._Id;
+        
+        const isDeleted = await deleteUserParking({ _Id: userParkingId });
+        if (!isDeleted) {
+            return res.status(500).json({message: "Delete parking error"});
+        }
+
+        return res.status(200).json({
+            message: 'Delete parking successfully',
+            isDeleted
+        });
+    
+    } catch(e) {
+        console.log(e);
+    }
+
+}
+
+module.exports = deleteParkingController;
