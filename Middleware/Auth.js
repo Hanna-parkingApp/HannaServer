68a1c638c2db81cf2088f3ff8ac7e345673af71d
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2022-06-11 18:09:15 +0300

    added auto login path

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 const config = process.env;
-const refreshToken = require('../Services/token.service')
+const refreshTokenService = require('../Services/token.service')
 const userService = require('../services/user.service');
 
 const verifyToken = async (req, res, next) => {
     const token = req.body.token || req.query.token || req.headers["x-access-token"];
+    const refreshtoken = req.body.refreshToken || req.query.refreshToken || req.headers["x-refresh-token"];
 
-    if (!token) {
+    if (!token || !refreshtoken) {
         return res.status(400).send("Token is required");
     }
 
@@ -19,11 +20,12 @@ const verifyToken = async (req, res, next) => {
             if (!req.headers["x-refresh-token"]) {
                 return res.status(400).send("Refresh token is required");
             }
-            const userArray = userService.getUser({token: req.headers["x-refresh-token"]});
-            const tokens = await refreshToken.refreshToken(req.headers["x-refresh-token"], userArray[0]);
+            const userArray = await userService.getUser({token: req.headers["x-refresh-token"]});
+            const tokens = await refreshTokenService.refreshToken(req.headers["x-refresh-token"], userArray[0]);
             req.headers["x-refresh-token"] = tokens.refreshToken;
             req.headers["x-access-token"] = tokens.accessToken;
             req.user = userArray[0];
+            req.flag = true;
             return next();
         }
         return res.status(400).send("invalid token");
