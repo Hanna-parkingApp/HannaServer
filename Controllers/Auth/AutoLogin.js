2e1d6ced5b9d281aa2994c3219c71074728a9398
hanna_user <dbn290895@gmail.com>
2022-06-13 20:37:04 +0300

    auto login tweeks

--- Controllers/Auth/AutoLogin.js
+++ Controllers/Auth/AutoLogin.js
@@ -1,7 +1,19 @@
+const { getUser } = require('../../Services/user.service');
+
 async function autoLoginController(req, res) {
+    console.log("AutoLoginController");
     try {
+        console.log("req.user: ", req.user);
+        const user = await getUser({ email: req.user.userEmail })
+
+        if (!user) {
+            return res.status(500).send('User not found by email');
+        }
+
+        console.log(user);
+
         return res.status(200).json({
-            user: req.user,
+            user: user[0],
             RefreshToken: req.headers["x-refresh-token"],
             AccessToken: req.headers["x-access-token"]
         })
